0382f3994d5a962821eb46b2091d17264febd7b4
Samarth120402 <124183347+Samarth120402@users.noreply.github.com>
2025-02-12 14:03:20 +0530

    Update fixed.cpp

--- Day 1/IntOverflow/fixed.cpp
+++ Day 1/IntOverflow/fixed.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <limits>
-
-int add(int a, int b) {
-    if((b<0 && a> std::numeric_limits<int>::max() -b) ||
-    (b<0 && a< std::numeric_limits<int>::min() -b))
+ 
+bool safe_add(int a, int b, int &result) {
+    if((b>0 && a> std::numeric_limits<int>::max()-b) || (b<0 && a< std::numeric_limits<int>::min()-b)) 
     {
         return false;
     }
-    result = a+b;
-    return false
+    result =a+b;
+    return true;
 }
-
+ 
 int main() {
-    int x,y;
+    //int x = 2000000000000;
+    //int y = 1500000000000;
+    //int result;
+    int x,y,result;
     std::cout << "Enter the first integer (x): ";
     std::cin >> x;
     std::cout << "Enter the second integer (y): ";
     std::cin >> y;
-   if 
-    std::cout << "Result: " << result << std::endl;
+    if(safe_add(x,y,result))
+    {
+        std::cout << "Result: "<< result << std::endl;
+    }
+    else
+    {
+        std::cout<<"Integer Overflow";
+    }
     return 0;
+
 }
